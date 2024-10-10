8548f808a4766a5e3ece23059195ffcc1c20c5e9
Leo53131 <2520221021@coreducacion.edu.co>
2024-10-09 20:15:13 -0500

    olvidaste tu contraseña
    las dos primeras interfases

---
+++ VISTA/OlvidasteContraseña.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles_OlvidasteContraseña.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="contenedor">
+        <div class="box">
+            <div class="encabezado">
+            <h2>¿Olvidaste tu contraseña?</h2>
+            <h5>Recupera tu cuenta</h5>
+            
+            </div>
+
+        <form>
+            <div>
+            <h4>Correo electrónico</h4>
+            <input type="email" name="correo" placeholder="Ejemplo@gmail.com">
+            </div>
+        
+        <button type="submit" class="envia">Enviar</button>
+        </form>
+        </div>
+
+    </div>
+   
+</body>
+</html>
