c6dabf35438e427dfeb1c0f0f28f9ed338649c74
Leo53131 <2520221021@coreducacion.edu.co>
2024-10-12 17:26:05 -0500

    actualizacion
    con recuperacion de contraseña, aviso de recuperacion de contraseña y correccion de inicio de sesio
    
    falta otras dos y las interfaces principales

--- VISTA/OlvidasteContraseña.php
+++ VISTA/OlvidasteContraseña.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles_OlvidasteContraseña.css">
-    <title>Document</title>
+    
+    <title>Olvidaste Contraseña</title>
 </head>
 <body>
     <div class="contenedor">
@@ -12,13 +13,13 @@
             <div class="encabezado">
             <h2>¿Olvidaste tu contraseña?</h2>
             <h5>Recupera tu cuenta</h5>
-            
             </div>
 
         <form>
             <div>
             <h4>Correo electrónico</h4>
             <input type="email" name="correo" placeholder="Ejemplo@gmail.com">
+            
             </div>
         
         <button type="submit" class="envia">Enviar</button>
--- VISTA/inicio_sesion.php
+++ VISTA/inicio_sesion.php
@@ -5,42 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AdminiPark - Iniciar sesión</title>
     <link rel="stylesheet" href="styles_InicioSesion.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <div class="login-container">
         <div class="login-box">
             <div class="logo">
-            <h1>ADMINIPARK</h1>
-            <div class="icon-logo"></div>
+                <h1>ADMINIPARK</h1>
+                <div class="icon-logo">
+                    <img src="logo.png" alt="fondo">
+                </div>
             </div>
             <h2>¡Bienvenido!</h2>
             <form>
-            <div class="input-group">
-                <i class="fas fa-users"></i>
-                <select>
-                    <option value="">Seleccione un rol...</option>
-                    <option value="admin">Administrador</option>
-                    <option value="user">Usuario</option>
-                </select>
-            </div>
-            <div class="input-group">
-                <i class="fas fa-user"></i>
-                <input type="text" placeholder="Usuario o correo">
-            </div>
-            <div class="input-group">
-                <i class="fas fa-key"></i>
-                <input type="password" placeholder="Contraseña">
-                <i class="fas fa-eye-slash" id="togglePassword"></i>
-            </div>
+                <div class="input-group">
+                    <i class="fas fa-users"></i>
+                    <select>
+                        <option value="">Seleccione un rol...</option>
+                        <option value="admin">Administrador</option>
+                        <option value="user">Usuario</option>
+                    </select>
+                </div>
+                <div class="input-group">
+                    <i class="fas fa-user"></i>
+                    <input type="text" placeholder="Usuario o correo">
+                </div>
+                <div class="input-group">
+                    <i class="fas fa-key"></i>
+                    <input type="password" placeholder="Contraseña">
+                    <i class="fas fa-eye-slash" id="togglePassword"></i>
+                </div>
                 <a href="#" class="forgot-password">¿Olvidaste tu contraseña?</a>
-                <button type="submit" class="login-button">Iniciar sesión</button>
+                <button type="submit" class="login-button"><b>Iniciar sesión</b></button>
             </form>
             <p class="register">¿Aún no tienes cuenta? <a href="#">Únete aquí</a></p>
-        </div>
-    </div>
-</body>
-</html>
-
-
