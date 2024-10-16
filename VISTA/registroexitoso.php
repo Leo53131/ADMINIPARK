f83bd2cf5598ff1ee89faba930d3fdc824031976
Leo53131 <2520221021@coreducacion.edu.co>
2024-10-16 10:05:47 -0500

    avance
    registro exitoso terminado

---
+++ VISTA/registroexitoso.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_AvisoRExitoso.css">
+    <title>Aviso Registro exitoso</title>
+</head>
+<body>
+    <div class="contenedor">
+        <div class="box">
+            <h1>¡Registro Exitoso!</h1>
+            <p>Tu cuenta ha sido creada cin exíto. Hemos enviado un correo de confirmacion
+                a <b>Ejemplo@gmail.com</b>. Por favor, revisa tu bandeja de entrada y sigue las instrucciones para verificar tu cuenta.
+            </p>
+            <p><b>Nota: Si no encentras el correo, verifica en tu carpeta de spam o correos 
+                no deseados.
+            </b></p>
+            <div class="botones">
+                <button type="submit"><b>Enviar de nuevo</b></button>
+                <button type="submit"><b>Ir a inicio de Sesion</b></button>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
