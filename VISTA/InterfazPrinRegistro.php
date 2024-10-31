f16c893340ef43c4bd9377ad0518e71617552b1f
Leo53131 <2520221021@coreducacion.edu.co>
2024-10-30 19:58:18 -0500

    avance
    intento de terminar la interfaz principal con funcionalidad temporal para registro de empleados y supuesto funcionamiento de la sidebar

--- VISTA/InterfazPrinRegistro.php
+++ VISTA/InterfazPrinRegistro.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="../estilos/style_InterfazPrinRegistro.css">
     <title>Interfaz Principal Admin</title>
+    <style>
+        .hidden {
+            display: none;
+        }
+    </style>
 </head>
 <body>
     <div class="parent">
@@ -31,147 +36,224 @@
        
         <div class="div2">
             <div class="sidebar">
-                <a href="#"><i class="fas fa-user"></i><span>Gestión de empleados</span></a>
-                <a href="#"><i class="fas fa-users"></i><span>Usuarios</span></a>
-                <a href="#"><i class="fas fa-car"></i><span>Vehículos</span></a>
-                <a href="#"><i class="fas fa-file-invoice"></i><span>Factura</span></a>
+                <a href="#" onclick="showSection('employees')"><i class="fas fa-user"></i><span>Gestión de empleados</span></a>
+                <a href="#" onclick="showSection('users')"><i class="fas fa-users"></i><span>Usuarios</span></a>
+                <a href="#" onclick="showSection('vehicles')"><i class="fas fa-car"></i><span>Vehículos</span></a>
+                <a href="#" onclick="showSection('invoices')"><i class="fas fa-file-invoice"></i><span>Factura</span></a>
                 <a href="inicio_sesion.php"><i class="fas fa-sign-out-alt"></i><span>Salir</span></a>
             </div>
         </div>
 
-        <div class="div3" id="content">
-            
+        <div class="div3">
+            <!-- Sección de Gestión de Empleados -->
+            <div id="employees" class="main-content hidden">
+                <h2 class="nunito-unique-600">Empleados</h2>
+                <hr class="separator-line">
+                
+                <div class="search-container">
+                    <input type="text" placeholder="Buscar empleados..." aria-label="Buscar empleados">
+                    <button type="button" class="nunito-unique-600" onclick="showRegisterForm()">Agregar nuevo empleado</button>
+                </div>
+
+                <div class="table-container">
+                    <h3 class="nunito-unique-600">Empleados registrados</h3>
+                    <p>Fecha de registro: <span id="registrationDate">16 de octubre de 2024</span></p>
+                    <table class="employees-table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Usuario</th>
+                                <th>Contraseña</th>
+                                <th>Rol</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody id="employeeTableBody">
+                            <!-- Las filas de empleados se agregarán aquí dinámicamente -->
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="pagination">
+                    <button class="nunito-unique-600">Anterior</button>
+                    <button class="nunito-unique-600">1</button>
+                    <button class="nunito-unique-600">2</button>
+                    <button class="nunito-unique-600">Siguiente</button>
+                </div>
+
+                <div id="registerEmployeeForm" class="hidden">
+                    <h2>Formulario de Registro de Empleado</h2>
+                    <div class="separator-line"></div>
+                    <div class="form-container">
+                        <h3 style="text-align: center;">Usuario</h3>
+                        <input type ="text" id="username" placeholder="Usuario" class="nunito-unique-200" oninput="saveFormData()">
+                        
+                        <h3 style="text-align: center;">Contraseña</h3>
+                        <div class="password-container">
+                            <input type="password" id="password" placeholder="Contraseña" oninput="saveFormData()">
+                            <i class="fas fa-eye-slash" id="togglePassword" style="cursor: pointer;"></i>
+                        </div>
+                        
+                        <h3 style="text-align: center;">Rol</h3>
+                        <select id="role" class="nunito-unique-200" onchange="saveFormData()">
+                            <option>-- Seleccione un rol --</option>
+                            <option>Admin</option>
+                            <option>Empleado</option>
+                        </select>
+                        
+                        <button type="submit" class="nunito-unique-600" onclick="registerEmployee()">Registrar</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Sección de Usuarios -->
+            <div id="users" class="main-content hidden">
+                <h2 class="nunito-unique-600">Usuarios</h2>
+                <hr class="separator-line">
+                
+                <div class="search-container">
+                    <input type="text" placeholder="Buscar usuarios..." aria-label="Buscar usuarios">
+                </div>
+
+                <div class="table-container">
+                    <h3 class="nunito-unique-600">Usuarios registrados</h3>
+                    <p>Fecha de registro: 16 de octubre de 2024</p>
+                    <table class="users-table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Email</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Las filas de usuarios se agregarán aquí dinámicamente -->
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="pagination">
+                    <button class="nunito-unique-600">Anterior</button>
+                    <button class="nunito-unique-600">1</button>
+                    <button class="nunito-unique-600">2</button>
+                    <button class="nunito-unique-600">Siguiente</button>
+                </div>
+            </div>
+
+            <!-- Sección de Vehículos -->
+            <div id="vehicles" class="main-content hidden">
+                <h2 class="nunito-unique-600">Vehículos</h2>
+                <hr class="separator-line">
+                
+                <div class="search-container">
+                    <input type="text" placeholder="Buscar vehículos..." aria-label="Buscar vehículos">
+                </div>
+
+                <div class="table-container">
+                    <h3 class="nunito-unique-600">Vehículos registrados</h3>
+                    <p>Fecha de registro: 16 de octubre de 2024</p>
+                    <table class="vehicles-table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Matrícula</th>
+                                <th>Marca</th>
+                                <th>Modelo</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Las filas de vehículos se agregarán aquí dinámicamente -->
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="pagination">
+                    <button class="nunito-unique-600">Anterior</button>
+                    <button class="nunito-unique-600">1</button>
+                    <button class="nunito-unique-600">2</button>
+                    <button class="nunito-unique-600">Siguiente</button>
+                </div>
+            </div>
+
+            <!-- Sección de Facturas -->
+            <div id="invoices" class="main-content hidden">
+                <h2 class="nunito-unique-600">Facturas</h2>
+                <hr class="separator-line">
+                
+                <div class="search-container">
+                    <input type="text" placeholder="Buscar facturas..." aria-label="Buscar facturas">
+                </div>
+
+                <div class="table-container">
+                    <h3 class="nunito-unique-600">Facturas registradas</h3>
+                    <p>Fecha de registro: 16 de octubre de 2024</p>
+                    <table class="invoices-table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Número de factura</th>
+                                <th>Fecha de emisión</th>
+                                <th>Monto</th >
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Las filas de facturas se agregarán aquí dinámicamente -->
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="pagination">
+                    <button class="nunito-unique-600">Anterior</button>
+                    <button class="nunito-unique-600">1</button>
+                    <button class="nunito-unique-600">2</button>
+                    <button class="nunito-unique-600">Siguiente</button>
+                </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
 
-<script>
-    // Función para cambiar el contenido de div3
-    function changeContent(content) {
-        document.getElementById('content').innerHTML = content;
+        <script>
+            let employeeCount = 1;
 
-        // Agregar la funcionalidad de mostrar/ocultar contraseña
-        const togglePassword = document.getElementById('togglePassword');
-        const passwordInput = document.getElementById('password');
-
-        if (togglePassword && passwordInput) {
-            togglePassword.addEventListener('click', function () {
-                // Cambia el tipo de input entre 'password' y 'text'
-                const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-                passwordInput.setAttribute('type', type);
-                // Cambia el ícono del ojo
-                this.classList.toggle('fa-eye-slash');
-                this.classList.toggle('fa-eye');
-            });
-        }
-
-        // Cargar datos del formulario desde localStorage
-        loadFormData();
-    }
-
-    // Función para guardar datos del formulario en localStorage
-    function saveFormData() {
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        const role = document.getElementById('role').value;
-
-        localStorage.setItem('username', username);
-        localStorage.setItem('password', password);
-        localStorage.setItem('role', role);
-    }
-
-    // Función para cargar datos del formulario desde localStorage
-    function loadFormData() {
-        const username = localStorage.getItem('username') || '';
-        const password = localStorage.getItem('password') || '';
-        const role = localStorage.getItem('role') || '-- Seleccione un rol --';
-
-        document.getElementById('username').value = username;
-        document.getElementById('password').value = password;
-        document.getElementById('role').value = role;
-    }
-
-    // Agregar eventos a los enlaces de la barra lateral
-    document.querySelectorAll('.sidebar a').forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Evitar el comportamiento por defecto del enlace
-            const content = this.querySelector('span').textContent; // Obtener el texto del enlace
-
-            // Cambiar el contenido según el enlace
-            let newContent;
-            switch (content) {
-                case 'Gestión de empleados':
-                    newContent = `
-                        <h2>Formulario de Registro</h2>
-                        <div class="separator-line"></div>
-                        <div class="form-container">
-                            <h3 style="text-align: center;">Usuario</h3>
-                            <input type="text" id="username" placeholder="Usuario" class="nunito-unique-200" oninput="saveFormData()">
-                            
-                            <h3 style="text-align: center;">Contraseña</h3>
-                            <div class="password-container">
-                                <input type="password" id="password" placeholder="Contraseña" oninput="saveFormData()">
-                                <i class="fas fa-eye-slash" id="togglePassword" style="cursor: pointer;"></i>
-                            </div>
-                            
-                            <h3 style="text-align: center;">Rol</h3>
-                            <select id="role" class="nunito-unique-200" onchange="saveFormData()">
-                                <option>-- Seleccione un rol --</option>
-                                <option>Admin</option>
-                                <option>Empleado</option>
-                            </select>
-                            
-                            <button type="submit" class="nunito-unique-600">Registrar</button>
-                        </div>
-                    `;
-                    break;
-                case 'Usuarios':
-                    newContent = '<h2>Usuarios</h2><p>Aquí puedes gestionar a los usuarios.</p>';
-                    break;
-                case 'Vehículos':
-                    newContent = '<h2>Vehículos</ h2><p>Aquí puedes gestionar los vehículos.</p>';
-                    break;
-                case 'Factura':
-                    newContent = '<h2>Factura</h2><p>Aquí puedes gestionar las facturas.</p>';
-                    break;
-                case 'Salir':
-                    window.location.href = 'inicio_sesion.php'; // Redirigir a inicio_sesion.php
-                    break;
+            function showSection(sectionId) {
+                const sections = document.querySelectorAll('.main-content');
+                sections.forEach(section => section.classList.add('hidden'));
+                document.getElementById(sectionId).classList.remove('hidden');
             }
 
-            changeContent(newContent); // Cambiar el contenido de div3
-        });
-    });
+            function showRegisterForm() {
+                document.getElementById('registerEmployeeForm').classList.remove('hidden');
+            }
 
-    // Mostrar automáticamente "Gestión de empleados" al cargar la página
-    document.addEventListener('DOMContentLoaded', function() {
-        const defaultContent = `
-            <h2>Formulario de Registro</h2>
-            <div class="separator-line"></div>
-            <div class="form-container">
-                <h3 style="text-align: center;">Usuario</h3>
-                <input type="text" id="username" placeholder="Usuario" class="nunito-unique-200" oninput="saveFormData()">
-                
-                <h3 style="text-align: center;">Contraseña</h3>
-                <div class="password-container">
-                    <input type="password" id="password" placeholder="Contraseña" oninput="saveFormData()">
-                    <i class="fas fa-eye-slash" id="togglePassword" style="cursor: pointer;"></i>
-                </div>
-                
-                <h3 style="text-align: center;">Rol</h3>
-                <select id="role" class="nunito-unique-200" onchange="saveFormData()">
-                    <option>-- Seleccione un rol --</option>
-                    <option>Admin</option>
-                    <option>Empleado</option>
-                </select>
-                
-                <button type="submit" class="nunito-unique-600">Registrar</button>
-            </div>
-        `;
-        changeContent(defaultContent); // Mostrar automáticamente "Gestión de empleados"
-    });
-</script>
-</body>
+            function registerEmployee() {
+                const username = document.getElementById('username').value;
+                const password = document.getElementById('password').value;
+                const role = document.getElementById('role').value;
+
+                const newRow = document.createElement('tr');
+                newRow.innerHTML = `
+                    <td>${employeeCount}</td>
+                    <td>${username}</td>
+                    <td>******</td>
+                    <td>${role}</td>
+                    <td class="action-buttons">
+                        <i class="fas fa-edit" title="Editar"></i>
+                        <i class="fas fa-trash" title="Eliminar"></i>
+                        <i class="fas fa-pencil-alt" title="Modificar"></i>
+                    </td>
+                `;
+
+                document.getElementById('employeeTableBody').appendChild(newRow);
+                employeeCount++;
+
+                document.getElementById('registerEmployeeForm').classList.add('hidden');
+                document.getElementById('username').value = '';
+                document.getElementById('password').value = '';
+                document.getElementById('role').value = '-- Seleccione un rol --';
+            }
+        </script>
+    </body>
 </html>
